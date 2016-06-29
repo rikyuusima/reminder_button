0e3cc29b60f9a633be7cdf20686303aaec5a674d
rikyuusima <equanz324@gmail.com>
2016-06-29 18:51:31 +0900

    :memo:add 時刻とリスト

--- Reminder_Button/app/src/main/java/com/example/user/reminder_button/MainActivity.java
+++ Reminder_Button/app/src/main/java/com/example/user/reminder_button/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.user.reminder_button;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
 import android.view.KeyEvent;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean dispatchKeyEvent(KeyEvent e) {
         //Sound_UPDOWN_Button
         if (e.getAction() == KeyEvent.ACTION_DOWN){
-            Toast.makeText(this, "押した", Toast.LENGTH_LONG).show();
+            Date date = new Date();
+            SimpleDateFormat hour = new SimpleDateFormat("HH");
+            SimpleDateFormat minute = new SimpleDateFormat("mm");
+            String hour_str = hour.format(date);
+            String minute_str = minute.format(date);
+            Toast.makeText(this, hour_str + ":" + minute_str, Toast.LENGTH_LONG).show();
+            ListViewCompat list = (ListViewCompat)findViewById(R.id.);
         }
         return true;
     }
